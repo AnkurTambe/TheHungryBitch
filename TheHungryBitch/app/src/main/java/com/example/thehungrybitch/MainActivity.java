2ed8e0ac57fefb8c91b8d6e66d3541f438f8cec2
AnkurTambe <201951025@iiitvadodara.ac.in>
2021-01-05 17:56:58 +0530

    adding the folder

---
+++ TheHungryBitch/app/src/main/java/com/example/thehungrybitch/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.thehungrybitch;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView starterCard;
+    CardView mainsCard;
+    CardView desertsCard;
+    TextView emailAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        starterCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
+
+        mainsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainsActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
+                startActivity(mainsActivityIntent);
+            }
+        });
+
+        desertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent desertsActivityIntent = new Intent(MainActivity.this, DesertsActivity.class);
+                startActivity(desertsActivityIntent);
+            }
+        });
+
+        emailAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailAddressIntent = new Intent(Intent.ACTION_SENDTO);
+                emailAddressIntent.setData(Uri.parse("mailto:thehungrybitch@example.com"));
+                startActivity(emailAddressIntent);
+            }
+        });
+    }
+
+    private void findViews() {
+        starterCard = findViewById(R.id.card_starters);
+        mainsCard = findViewById(R.id.card_mains);
+        desertsCard = findViewById(R.id.card_deserts);
+        emailAddress = findViewById(R.id.email_address);
+    }
+}
