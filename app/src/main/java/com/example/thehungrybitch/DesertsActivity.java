cf912a239eb007f3ece807c1539a887fa12bb040
AnkurTambe <201951025@iiitvadodara.ac.in>
2021-06-02 23:35:14 +0530

    portrait

--- app/src/main/java/com/example/thehungrybitch/DesertsActivity.java
+++ app/src/main/java/com/example/thehungrybitch/DesertsActivity.java
@@ -1,10 +1,10 @@
 package com.example.thehungrybitch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DesertsActivity extends AppCompatActivity {
 
